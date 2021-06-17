7a63533d24007945e3074afff425c09c2de7a267
mworld7 <mworld7@gmail.com>
2021-06-17 22:27:09 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,12 @@ class Item < ApplicationRecord
   vakidates :title, presence: true
   validates :price, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_by_id, presence: true
-  validates :ship_from_id, presence: true
-  validates :shipping_date_id, presence: true
+  # validates :category_id, presence: true
+  # validates :condition_id, presence: true
+  # validates :postage_by_id, presence: true
+  # validates :ship_from_id, presence: true
+  # validates :shipping_date_id, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
 
   belongs_to :user
   has_one_attached :image
