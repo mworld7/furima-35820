3978c8aec60594538dd2163927464cb71af819f7
mworld7 <mworld7@gmail.com>
2021-06-17 22:19:49 +0900

    アソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,11 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage_by
+  belongs_to :ship_from
+  belongs_to :shipping_date
   
 end
