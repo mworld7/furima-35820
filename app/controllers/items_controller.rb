484526f5d70c57803ac958afa4828f003c050309
mworld7 <mworld7@gmail.com>
2021-06-18 13:57:57 +0900

    商品出品ページを表示できるように

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user! only: [:new, :create]
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
-    @item = Items.new
+    @item = Item.new
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
+
+  def item_params
     params.require(:item).permit(:title, :price, :image, :description, :category_id, :condition_id, :postage_by_id, :ship_from_id, :shipping_date_id).merge(user_id: current_user.id)
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,14 @@
 class Item < ApplicationRecord
   with_options presence: true do 
-    vakidates :title, presence: true
-    validates :description, presence: true
-    validates :category_id, presence: true
-    validates :condition_id, presence: true
-    validates :postage_by_id, presence: true
-    validates :ship_from_id, presence: true
-    validates :shipping_date_id, presence: true
+    validates :title
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :postage_by_id
+    validates :ship_from_id
+    validates :shipping_date_id
   end
-  validates :price, presence: true, numericality: {in:300..9999999}, format: { with: /\A[0-9]+\z/ } #"presence: true" と "numericality:"は意味が重複する？ 
+  validates :price, presence: true, numericality: {in:300..9999999}, format: { with: /\A[0-9]+\z/ }  
   validates :genre_id, numericality: { other_than: 1 } 
 
   belongs_to :user
