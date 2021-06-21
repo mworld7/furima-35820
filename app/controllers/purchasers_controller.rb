981613c6a44e62ef6e1a19bc0e31351d52830a63
mworld7 <mworld7@gmail.com>
2021-06-21 14:36:40 +0900

    商品購入ページに遷移できるように。

--- app/controllers/purchasers_controller.rb
+++ app/controllers/purchasers_controller.rb
@@ -1,3 +1,11 @@
 class PurchasersController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:index, :create]
+
+  def index
+   
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-    resources :purchasers, only: :create 
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :purchasers, only: [:index, :create] 
+  end
 end
