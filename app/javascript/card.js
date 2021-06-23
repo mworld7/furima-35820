afba424b8d7f97b9b5093a98f4fe9374013511f1
mworld7 <mworld7@gmail.com>
2021-06-23 20:02:30 +0900

    PAY.JPのテスト公開鍵を取得し設定/トークン化の処理

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_b8b4a8092130bed3c636b3fb");
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchaser_address[number]"),
+      exp_month: formData.get("purchaser_address[exp_month]"),
+      exp_year: `20${formData.get("purchaser_address[exp_year]")}`,
+      cvc: formData.get("purchaser_address[cvc]"),
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
